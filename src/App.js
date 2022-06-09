abbcce863f85090b94480b19a5cfb70821be3f66
brandonblack <brandonblack02@gmail.com>
2022-06-08 19:29:47 -0700

    Some more setup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
+import axios from 'axios';
 import './App.css';
 
 function App() {
+
+  let getAuth = () => {
+    let config = {
+      headers: {
+        "Content-Type": "application/json"
+      }
+    }
+
+    let data = {
+      "password": ""
+    }
+
+    axios.post("https://owner-api.teslamotors.com/oauth/token", data, config).then((response) => {
+      console.log(response)
+    })
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Testing App</h1>
     </div>
   );
 }
