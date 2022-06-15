153ae21d57462c79e5c01a87a84005e04e4aa35d
Brandon Black <53108034+Brandon205@users.noreply.github.com>
2022-06-14 21:01:59 -0700

    Tried TeslaJS, but it's outdated

--- src/App.js
+++ src/App.js
@@ -1,31 +1,30 @@
-import React, { useState } from 'react';
-import axios from 'axios';
-import env from 'react-dotenv';
-import TestPage from './TestPage.js';
+import React from 'react';
+// import axios from 'axios';
+// import env from 'react-dotenv';
 import './App.css';
 
 function App() {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+  // const [email, setEmail] = useState("");
+  // const [password, setPassword] = useState("");
 
-  let getAuth = () => {
-    let config ={
-      headers: {
-        "Access-Control-Allow-Origin": "*",
-      }
-    }
+  // let getAuth = () => {
+  //   let config ={
+  //     headers: {
+  //       "Access-Control-Allow-Origin": "*",
+  //     }
+  //   }
   
 
-    let data = {
-      'client_id': 'ownerapi',
-      'code_challenge': '123',
-      'code_challenge_method': 'S256',
-      'redirect-uri': 'https://auth.tesla.com/void/callback',
-      'response_type': 'code',
-      'scope': "openid email offline_access",
-      'state': '12345',
-      'login_hint': email
-    }
+    // let data = {
+    //   'client_id': 'ownerapi',
+    //   'code_challenge': '123',
+    //   'code_challenge_method': 'S256',
+    //   'redirect-uri': 'https://auth.tesla.com/void/callback',
+    //   'response_type': 'code',
+    //   'scope': "openid email offline_access",
+    //   'state': '12345',
+    //   'login_hint': email
+    // }
 
     // axios.post("https://auth.tesla.com/oauth2/v3/authorize", JSON.stringify(data), config).then((response) => {
     //   console.log(response)
@@ -44,12 +43,12 @@ function App() {
     // })
 
     // axios.get('https://auth.tesla.com/oauth2/v3/authorize')
-    fetch('https://auth.tesla.com/oauth2/v3/authorize').then((res) => {
-      console.log(res)
-    });
+    // fetch('https://auth.tesla.com/oauth2/v3/authorize').then((res) => {
+    //   console.log(res)
+    // });
 
     // console.log(JSON.stringify(config), JSON.stringify(data))
-  }
+  // }
 
   return (
     <div className="App">
@@ -57,7 +56,6 @@ function App() {
       <input value={email} onChange={(e) => setEmail(e.target.value)} placeholder="Email"></input>
       <input value={password} onChange={(e) => setPassword(e.target.value)} placeholder="Password" type="password"></input>
       <button onClick={() => getAuth()}>Get Auth</button> */}
-      <TestPage />
     </div>
   );
 }
