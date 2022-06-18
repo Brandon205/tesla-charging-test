c90096a8dc7e0f7721dedd65493210f1d1d2d325
brandonblack <brandonblack02@gmail.com>
2022-06-17 19:51:54 -0700

    Added new route and button

--- src/TestPage.js
+++ src/TestPage.js
@@ -14,6 +14,15 @@ export default function TestPage() {
         })
     }
 
+    let getVehicle = () => {
+        let id = '1492931281739069'
+
+        axios.get(`/vehicles/${id}`).then((response) => {
+            console.log(response.data)
+            setData(response.data)
+        })
+    }
+
     let getVehicleChargeState = () => {
         // For now just use a hardcoded vehicle ID but later can create buttons that pass in the ID
         // let id = '1492931281739069'
@@ -28,6 +37,7 @@ export default function TestPage() {
         <div>
             <h1>Hello</h1>
             <button onClick={() => {getVehicles()}}>Get Vehicles</button>
+            <button onClick={() => {getVehicle()}}>Get Vehicle</button>
             <button onClick={() => {getVehicleChargeState()}}>Get Charge State</button>
 
         </div>
